2c1d4f1fe795484332b6343ec756d7fc479c043f
Amber Thomas <ProQuestionAsker@gmail.com>
2019-03-01 11:57:15 -0800

    fixed styling for post pages

--- theme/themes/data-science/static/js/theme.js
+++ theme/themes/data-science/static/js/theme.js
@@ -1,39 +1,49 @@
 let lightDark = 'light'
 let body = document.getElementsByTagName('body')[0]
 let lightDarkToggle = document.getElementsByClassName('theme-toggle')[0]
-console.log({lightDarkToggle})
+let currentTime = new Date().getHours()
+let store = sessionStorage['theme']
+if (store){
+  const theme = sessionStorage['theme']
+  body.classList.add(`${theme}`)
+} if (!store){
+  setTheme()
+}
 
 // setup light dark theme toggle
 lightDarkToggle.addEventListener('click', handleThemeToggle)
 
 
 function handleThemeToggle(){
+  console.log({store})
   let icon = this.getElementsByTagName('i')[0]
-  console.log({lightDark})
-  if (lightDark === 'light') {
-    lightDark = 'dark'
-    console.log({lightDark})
+  if (store === 'light') {
+    sessionStorage['theme'] = 'dark'
     body.classList.remove('light')
     body.classList.add('dark')
     icon.classList.remove('fa-moon')
     icon.classList.add('fa-circle')
   }
 
-  else if (lightDark === 'dark') {
-    lightDark = 'light'
+  else if (store === 'dark') {
+    sessionStorage['theme'] = 'light'
     body.classList.remove('dark')
     body.classList.add('light')
     icon.classList.remove('fa-circle')
     icon.classList.add('fa-moon')
   }
-  localStorage['theme'] = lightDark
-  let theme = localStorage['theme']
-  console.log({theme})
+  store = sessionStorage['theme']
+  // sessionStorage['theme'] = lightDark
+  // let theme = sessionStorage['theme']
+  //console.log({theme})
 }
 
 function setTheme(){
-  let store = localStorage['theme']
-  localStorage['theme'] = 'light'
-}
+  store = sessionStorage['theme']
 
-setTheme()
+  if (currentTime < 6 || currentTime >= 21) sessionStorage['theme'] = 'dark'
+  else sessionStorage['theme'] = 'light'
+  console.log({currentTime})
+
+  handleThemeToggle()
+}
